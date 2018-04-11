a40dd96b42a02a14ddc1689002a06b4fd887843b
James Whitney <jamesdrums@mac.com>
2018-04-11 15:57:40 -0400

    feat: Creates method to request a Token from API
    This function is part of the TMDBConvenience file which is an
    extension of the TMDBClient class. This function will eventually
    replace existing function found in LoginViewController. Again this is
    part of an exercise to adhere to the MVC design.

--- TheMovieManager/TMDBConvenience.swift
+++ TheMovieManager/TMDBConvenience.swift
@@ -74,9 +74,23 @@ extension TMDBClient {
     private func getRequestToken(_ completionHandlerForToken: @escaping (_ success: Bool, _ requestToken: String?, _ errorString: String?) -> Void) {
         
         /* 1. Specify parameters, the API method, and the HTTP body (if POST) */
+        let parameters = [String:AnyObject]()
+        
         /* 2. Make the request */
-        /* 3. Send the desired value(s) to completion handler */
-
+        let _ = taskForGETMethod(Methods.AuthenticationTokenNew, parameters: parameters) { ( results, error) in
+            
+            /* 3. Send the desired value(s) to completion handler */
+            if let error = error {
+                print(error)
+                completionHandlerForToken(false, nil, "Loging Failed (Request Token)")
+            } else {
+                if let requestToken = results?[TMDBClient.JSONResponseKeys.RequestToken] as? String {
+                    completionHandlerForToken(true, requestToken, nil)
+                } else {
+                    print("Key '\(TMDBClient.JSONResponseKeys.RequestToken)' not found in \(results!)")
+                }
+            }
+        }
     }
     
     private func loginWithToken(_ requestToken: String?, hostViewController: UIViewController, completionHandlerForLogin: @escaping (_ success: Bool, _ errorString: String?) -> Void) {
